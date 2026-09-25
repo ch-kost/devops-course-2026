4c4b2694bef1581082bd27b66eea95270066c499
ch-kost <ch-kost@users.noreply.github.com>
2026-09-25 16:34:33 +0300

    feat: add experimental multiply and divide

--- calculator.py
+++ calculator.py
@@ -10,3 +10,15 @@ def add(left: float, right: float) -> float:
 def subtract(left: float, right: float) -> float:
     """Return the difference of two numbers."""
     return left - right
+
+
+def multiply(left: float, right: float) -> float:
+    """Return the product of two numbers."""
+    return left * right
+
+
+def divide(left: float, right: float) -> float:
+    """Return the quotient of two numbers."""
+    if right == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return left / right
