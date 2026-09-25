e6f01086e43d24de7421d7e352551b690d185166
ch-kost <ch-kost@users.noreply.github.com>
2026-09-25 16:30:19 +0300

    feat: implement subtract function

--- calculator.py
+++ calculator.py
@@ -2,3 +2,8 @@ def add(left: float, right: float) -> float:
     """Return the sum of two numbers."""
     # fixed
     return left + right
+
+
+def subtract(left: float, right: float) -> float:
+    """Return the difference of two numbers."""
+    return left - right
